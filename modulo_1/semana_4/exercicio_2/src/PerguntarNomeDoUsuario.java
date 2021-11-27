13df0c732aa47c2b08e23c704d4a1a2b9a1fcbf7
Júlia Amaral <julia.amaral@sc.senai.br>
2021-11-27 18:47:30 -0300

    Exercício 2.

--- modulo_1/semana_4/exercicio_2/src/PerguntarNomeDoUsuario.java
+++ modulo_1/semana_4/exercicio_2/src/PerguntarNomeDoUsuario.java
@@ -1,21 +1,22 @@
-// Crie um programa em Java que, utilizando a classe Scanner, pergunte ao usuário seu sobrenome, depois seu nome, e então escreva na tela, em uma única “string”, o nome completo do usuário.
-// Informe também ao usuário quantas letras tem no nome dele.
-
 import java.util.Scanner;
 
 public class PerguntarNomeDoUsuario {
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Qual é o seu sobrenome? ");
-        String sobrenomne = input.nextLine();
+        String sobrenomne = input.nextLine().trim();
 
         System.out.println("Qual é o seu primeiro nome?");
-        String nome = input.nextLine();
+        String primeiroNome = input.nextLine().trim();
 
-        System.out.println(nome + " " + sobrenomne + ", seu nome possui " + nome.length() + " letras.");
+        String nome = primeiroNome + " " + sobrenomne;
+
+        int length = nome.length() - 1;
+
+        System.out.printf("Olá %s, seu nome possui %s letras.", nome, length);
     }
-
 
 }
