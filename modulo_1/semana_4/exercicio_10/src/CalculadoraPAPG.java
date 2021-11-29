0b0e22656b4afd89de9c70a5f6f17f8956ec1c69
Júlia Amaral <julia.amaral@sc.senai.br>
2021-11-28 22:08:17 -0300

    Exercício 10.

---
+++ modulo_1/semana_4/exercicio_10/src/CalculadoraPAPG.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class CalculadoraPAPG {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("%nPor favor, inserir um número inteiro: ");
+        double numeroInserido = input.nextInt();
+
+        System.out.printf("%nPor favor, inserir o valor da raiz: ");
+        double raiz = input.nextInt();
+
+        System.out.printf("%nVocê quer os 10 primeiros termos de uma PA ou de uma PG?%n");
+        System.out.println("1 - Progressão Aritmética");
+        System.out.println("2 - Progressão Geométrica");
+
+        int operacao;
+
+        do {
+            System.out.print("Por favor, digite o código desejável: ");
+            operacao = input.nextInt();
+        } while (!(operacao == 1 || operacao ==2));
+
+        double [] termos = new double[10];
+
+        String tipo = null;
+
+        switch (operacao) {
+            case 1:
+                for (int i = 0; i < termos.length; i++) {
+                    termos[i] = numeroInserido + (i * raiz);
+                }
+                tipo = "PA";
+                break;
+
+            case 2:
+                for (int i = 0; i < termos.length; i++) {
+                    termos[i] = numeroInserido * Math.pow(raiz, i);
+                }
+                tipo = "PG";
+        }
+
+        for (int i = 0; i < termos.length; i++) {
+            if (i == 0) {
+                System.out.printf("%nOs 10 primeiros termos da %s são: {%.0f", tipo, termos[0]);
+            } else {
+                System.out.printf(", %.0f", termos[i]);
+            }
+        }
+
+        System.out.println("}");
+
+    }
+}
