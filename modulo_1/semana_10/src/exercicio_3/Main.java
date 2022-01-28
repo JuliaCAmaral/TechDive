67f7133579c93e2eb25e7612dd0bd952ea54580d
Júlia Amaral <julia.amaral@sc.senai.br>
2022-01-28 19:41:26 -0300

    Exercícios 3.

---
+++ modulo_1/semana_10/src/exercicio_3/Main.java
@@ -0,0 +1,34 @@
+package exercicio_3;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class Main {
+    public static void main(String[] args) {
+        int currentYear = LocalDate.now().getYear();
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        LocalDate begin = LocalDate.of(currentYear, 1, 1);
+        TemporalAdjuster temporalAdjuster = TemporalAdjusters.next(DayOfWeek.SATURDAY);
+
+        if (begin.getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+            String beginFormat = begin.format(format);
+            System.out.println(beginFormat + "\n");
+        }
+
+        while (begin.getYear() == currentYear) {
+            LocalDate saturday = begin.with(temporalAdjuster);
+            String satudayFormat = saturday.format(format);
+
+            if (saturday.getYear() == currentYear) {
+                System.out.println(satudayFormat + "\n");
+                begin = saturday;
+
+            }
+        }
+    }
+}
---
+++ modulo_1/semana_10/src/exercicio_3/README.md
@@ -0,0 +1,3 @@
+# Exercício 3
+
+Escreva um programa que informe para o usuário a data, no formato “dd/mm/yyyy”, de todos os primeiros sábados de cada mês do ano corrente.
