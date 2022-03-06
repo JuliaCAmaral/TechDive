d8ca1f95f2223e9134cb6640d8ed1386e6730cec
Júlia Amaral <julia.amaral@sc.senai.br>
2022-03-06 13:01:13 -0300

    Adicionado mais dados.

--- modulo_2/semana_2/exercicio_7/Insercao_dados.sql
+++ modulo_2/semana_2/exercicio_7/Insercao_dados.sql
@@ -17,6 +17,8 @@ insert into university.cursos (nom_curso, cod_dpto) values ('Licenciado em Educa
 insert into university.cursos (nom_curso, cod_dpto) values ('Ciência da Informação', 			6);
 insert into university.cursos (nom_curso, cod_dpto) values ('Psicologia', 						7);
 insert into university.cursos (nom_curso, cod_dpto) values ('Bacharelado em Física', 			8);
+insert into university.cursos (nom_curso, cod_dpto) values ('Bacharelado em Matemática', 		8);
+insert into university.cursos (nom_curso, cod_dpto) values ('Bacharelado em Química', 			8);
 insert into university.cursos (nom_curso, cod_dpto) values ('Administração', 					9);
 insert into university.cursos (nom_curso, cod_dpto) values ('Engenharia Civil', 				10);
 
@@ -30,6 +32,26 @@ insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('G
 insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Ian Duarte', 		'2022-03-07', 8, 'n');
 insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Joana Fontes', 		'2021-03-07', 9, 'n');
 insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Mariana Oliveira',	'2020-08-25', 10, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Laura Fiúza', 		'2022-03-07', 1, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Timur Osório', 		'2021-08-25', 1, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Mohamed Dorneles', 	'2020-08-25', 1, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Ary Júdice', 		'2022-03-07', 1, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Cristovão Camelo', 	'2020-08-25', 2, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Indira Varejão', 	'2019-08-25', 2, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Emanuelle Gouveia',	'2018-08-25', 5, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Ivanilson Pastana', 	'2022-03-07', 5, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Nelsa Calheiros', 	'2021-03-07', 5, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Edir Toscano',		'2020-08-25', 4, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Edson Boeira', 		'2022-03-07', 4, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Zayn Prada', 		'2021-08-25', 4, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Zoe Aguiar', 		'2020-08-25', 4, 's');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Irene Lages', 		'2022-03-07', 6, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Patrícia Caldeira', 	'2020-08-25', 7, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Salomé Teles', 		'2019-08-25', 8, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Nuno Gadelha',		'2018-08-25', 8, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Aléxia Lucena', 		'2022-03-07', 11, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Ismael Lessa', 		'2021-03-07', 9, 'n');
+insert into university.alunos (nome, dat_entrada, cod_curso, cotista) values ('Mila Guedes',		'2020-08-25', 10, 's');
 
 insert into university.disciplinas (nome_disc, carga_horaria) values ('Anatomia Aplicada à Educação Física',	72);
 insert into university.disciplinas (nome_disc, carga_horaria) values ('Aprendizagem e Controle Motor',			72);
@@ -63,5 +85,25 @@ insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, st
 insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 8, 8, null, null, 	'C');
 insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (3, 9, 9, null, null, 	'C');
 insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 10, 10, 3.5, 10, 	'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 11, 4, 8.5, 3, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (2, 12, 4, 5.5, 5, 		'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 13, 5, 9, 2, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 14, 6, 4.5, 1,		'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 15, 7, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (6, 16, 8, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (8, 17, 8, 6.5, 8, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 18, 9, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (3, 19, 9, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 20, 9, 3.5, 10, 	'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 21, 10, 8.5, 3, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (2, 22, 10, 5.5, 5, 		'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 23, 2, 9, 2, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 24, 2, 4.5, 1,		'R');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 25, 2, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (6, 26, 2, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (8, 27, 3, 6.5, 8, 		'A');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (1, 28, 4, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (3, 29, 4, null, null, 	'C');
+insert into university.matriculas (semestre, mat_alu, cod_disc, nota, faltas, status) values (4, 30, 5, 3.5, 10, 	'R');
 
 
