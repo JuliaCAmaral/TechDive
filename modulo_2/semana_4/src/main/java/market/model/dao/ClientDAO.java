5ace9ace202f8d38a1daf02e96588263f455af84
Júlia Amaral <julia.amaral@sc.senai.br>
2022-03-18 21:30:37 -0300

    Criação do ClientDAO.

---
+++ modulo_2/semana_4/src/main/java/market/model/dao/ClientDAO.java
@@ -0,0 +1,49 @@
+package market.model.dao;
+
+import market.model.persistence.Client;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class ClientDAO {
+
+    private EntityManager entityManager;
+
+    public ClientDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void create(Client client) {
+        this.entityManager.persist(client);
+    }
+
+    public List<Client> listAll() {
+        String sql = "SELECT * FROM Client";
+        return this.entityManager.createNamedQuery(sql, Client.class).getResultList();
+    }
+
+    public List<Client> listByName(String name) {
+        String sql = "SELECT * FROM Client WHERE name =:name";
+        return this.entityManager.createNamedQuery(sql, Client.class)
+                .setParameter("name", name.toLowerCase())
+                .getResultList();
+    }
+
+    public Client update(Client client) {
+        return convertToMerge(client);
+    }
+
+    public void delete(Client client) {
+        Client productMerged = convertToMerge(client);
+        this.entityManager.remove(client);
+    }
+
+    private Client convertToMerge(Client client) {
+        return this.entityManager.merge(client);
+    }
+
+    public Client getById(Long id) {
+        return this.entityManager.find(Client.class, id);
+    }
+
+}
