e17bcde2ba2957a09d7428d13c6b099f9518cb33
Júlia Amaral <julia.amaral@sc.senai.br>
2022-04-22 21:07:37 -0300

    Exercício 1.

---
+++ modulo_2/semana_9/m2s7/src/main/java/projeto/entity/Endereco.java
@@ -0,0 +1,4 @@
+package projeto.entity;
+
+public class Endereco {
+}
---
+++ modulo_2/semana_9/m2s7/src/main/java/projeto/entity/Escola.java
@@ -0,0 +1,66 @@
+package projeto.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Escola {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_escola")
+    private Long id;
+
+    private String nome;
+
+    private Endereco endereco;
+
+    private Date dataDeCriacao;
+
+    private List<Turma> turmas = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(Date dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
+}
