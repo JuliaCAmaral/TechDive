bdbfd56323f657a75716eef32fe4e5123d56f9d7
Júlia Amaral <julia.amaral@sc.senai.br>
2022-05-02 09:31:47 -0300

    Exercício ~.

--- modulo_2/semana_10/src/main/java/br/senai/cadastrocursos/modelo/Cursos.java
+++ modulo_2/semana_10/src/main/java/br/senai/cadastrocursos/modelo/Cursos.java
@@ -1,18 +1,34 @@
 package br.senai.cadastrocursos.modelo;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Objects;
 
 public class Cursos {
 
+    @NotEmpty(message = "{campo.obrigatorio} : codigo")
     private String codigo;
+
+    @NotEmpty(message = "{campo.obrigatorio} : nome")
+    @Size(min = 2, max = 100, message = "{campo.invalido.tamanho} : nome")
+    private String nome;
+
+    @NotNull(message = "{campo.obrigatorio} : data de inicio")
     private LocalDate dataDeInicio;
+
+    @NotNull(message = "{campo.obrigatorio} : data de fim")
     private LocalDate dataDeFim;
 
-    @Override
-    public String toString() {
-        return String.format("CURSO: Código: %s - Data de Início: %s - Data de Fim: %s.",
-                codigo, dataDeInicio, dataDeFim);
+    public Cursos() {
+    }
+
+    public Cursos(String codigo, String nome, LocalDate dataDeInicio, LocalDate dataDeFim) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataDeInicio = dataDeInicio;
+        this.dataDeFim = dataDeFim;
     }
 
     @Override
@@ -36,6 +52,14 @@ public class Cursos {
         this.codigo = codigo;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public LocalDate getDataDeInicio() {
         return dataDeInicio;
     }
@@ -50,5 +74,11 @@ public class Cursos {
 
     public void setDataDeFim(LocalDate dataDeFim) {
         this.dataDeFim = dataDeFim;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CURSO: Código: %s - Data de Início: %s - Data de Fim: %s.",
+                codigo, dataDeInicio, dataDeFim);
     }
 }
